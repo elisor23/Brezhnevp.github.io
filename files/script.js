7d5f3be0e5b71d9ec146e3c248d2c4964bb6c9bb
elisor23 <42843777+elisor23@users.noreply.github.com>
2019-08-26 01:30:31 +0300

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.partners__slider').slick({
+	  infinite: true,
+	  centerMode: true,
+	  centerPadding: '0px',
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  accessibility: false,
+	  swipe: false
+  });
+});
